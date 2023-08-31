662560f8697db33635da3d09ab8c6396eae83cb6
cesarjakub <list>
2023-08-31 18:48:00 +0200

    feat: api calls

--- script.js
+++ script.js
@@ -9,11 +9,33 @@ $(document).ready(function(){
             },
             success: function (result) {
               console.log(result);
+              $(".karta").remove();
+              $("#infocantainer").append(pokeInfo(result.name, result.id, result.height, result.weight, result.types[0].type.name));
             },
             error: function (error) {
                 console.log(error);
             },
         });
     }
+
+    function pokeInfo(name, id, height, weight, type){
+        let text = `<div class="karta">
+        <h2 class="name" id="name">${name}</h2>
+        <h3 class="ID" id="ID">${id}</h3>
+        <h4>Height: ${height}</h4>
+        <h4>Weight: ${weight}</h4>
+        <h4>Type: ${type}</h4>
+    </div>`;
+        return text;
+    }
+
+    function getSearchInput(){
+        let val = $("#textbar").val();
+        return val.toLowerCase();
+    }
+
+    $("#submitbutton").on("click", function(){
+        console.log(getName(getSearchInput()));
+    });
 
 });
